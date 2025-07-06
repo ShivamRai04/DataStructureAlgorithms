174ea8a510365dcce682f758b0733836d625db97
ShivamRai04 <shivamrai.cs04@gmail.com>
2025-07-06 22:51:11 +0530

    Time: 1 ms (83.57%), Space: 42 MB (60.19%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -6,31 +6,26 @@ class Solution {
 
         List<List<Integer>> list = new ArrayList<>();
         list.add(Arrays.asList(1));
-    
-        if(numRows>=2)
-          list.add(Arrays.asList(1,1));
-
-        if(numRows <=2)
-          return list;
+       
+        if(numRows==1)
+           return list;
         
-        System.out.println(list);
-        List<Integer> prevRow = list.get(1);   
-        int i=3;
-        System.out.println("Before while loop");
-        while(i<=numRows) {
+        list.add(Arrays.asList(1,1));
+        List<Integer> prevRow = list.get(1);  
+
+        for(int i=3; i<=numRows; i++) {
             List<Integer> row = new ArrayList<>();
             row.add(1);
-            int j=1;
-            while(j<prevRow.size()){
-                row.add(prevRow.get(j) + prevRow.get(j-1));
-                j++;
+
+            for(int j=1; j<prevRow.size();j++) {
+              row.add(prevRow.get(j) + prevRow.get(j-1));
             }
+
             row.add(1);
-           list.add(row);
-           prevRow = row;
-           i++;
+            list.add(row);
+            prevRow = row;  
         }
-        
-        return list;
+
+        return list; 
     }
 }
